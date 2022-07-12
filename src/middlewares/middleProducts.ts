7072440dffaa193d737c213c01218c0bfeab6259
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-12 20:27:41 -0300

    Feat: Req-02 Adds middleware for amount validation

--- src/middlewares/middleProducts.ts
+++ src/middlewares/middleProducts.ts
@@ -12,6 +12,19 @@ const nameValid = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
-export default {
+const amountValid = (req: Request, res: Response, next: NextFunction) => {
+  const { amount } = req.body;
+  if (!amount) return res.status(400).json({ message: '"amount" is required' });
+  if (typeof (amount) !== 'string') {
+    return res.status(422).json({ message: '"amount" must be a string' });
+  };
+  if (amount.length <= 2) {
+    return res.status(422).json({ message: '"amount" length must be at least 3 characters long' });
+  }
+  next();
+};
+
+export {
   nameValid,
+  amountValid,
 };
