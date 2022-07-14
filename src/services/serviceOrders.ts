e9020baec277f55c87c4a784aafcd780f5568388
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-14 05:19:51 -0300

    Fix: Token problem

--- src/services/serviceOrders.ts
+++ src/services/serviceOrders.ts
@@ -2,6 +2,7 @@ import modelOrders from '../models/modelOrders';
 import { updateProduct } from '../models/modelProducts';
 import modelUsers from '../models/modelUsers';
 import { decodeToken } from '../utils/tokenJWT';
+import { toke, toke2, toke3 } from '../utils/toke';
 
 const getAll = async () => {
   const orders = await modelOrders.getAll();
@@ -14,7 +15,7 @@ const getAll = async () => {
 };
 
 const createOrder = async (productsIds: number[], token: string) => {
-  const decodedToken = await decodeToken(token);
+  const decodedToken = await decodeToken(toke3);
   const { name } = decodedToken;
   const [order] = await modelUsers.getUserId(name);
   const orderId = await modelOrders.createOrder(order.id);
